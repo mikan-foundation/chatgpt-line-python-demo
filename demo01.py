b320eb170298055602aa2a71daa4dc25e50b9b17
WataruNishimura <wataru.nishimura@irenkakotan.co.jp>
2023-04-25 19:54:48 +0900

    update: demoを追加

---
+++ demo01.py
@@ -0,0 +1,26 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import openai
+
+#Dotenvを初期化
+load_dotenv(verbose=True)
+
+#.envファイルのパスを指定：main.pyの場所＋.envファイル
+dotenv_path= join(dirname(__file__), ".env")
+# .envファイルを読み込み
+load_dotenv(dotenv_path=dotenv_path)
+
+#環境変数としてAPI KEYを読み込み
+API_KEY = os.environ.get("GPT_API_KEY")
+
+openai.api_key = API_KEY
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[
+    {"role":"user", "content": "ChatGPTって何ですか？教えてください"}
+    ]
+)
+
+print(response.choices[0]["message"]["content"].strip())
