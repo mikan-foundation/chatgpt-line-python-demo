7e3727ab9782402af0a7845cf091b199fdcd9d4b
WataruNishimura <wataru.nishimura@irenkakotan.co.jp>
2023-04-25 21:21:51 +0900

    update: 勉強会での内容を反映

--- demo01.py
+++ demo01.py
@@ -14,12 +14,15 @@ load_dotenv(dotenv_path=dotenv_path)
 #環境変数としてAPI KEYを読み込み
 API_KEY = os.environ.get("GPT_API_KEY")
 
-openai.api_key = API_KEY
+openai.api_key = API_KEY #初期設定完了
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
     messages=[
-    {"role":"user", "content": "ChatGPTって何ですか？教えてください"}
+    {"role":"user", "content": "きのこの山とたけのこの里どっちがおいしいですか"},
+    {"role": "assistant", "content": "私は味覚を持っていないため、おいしいかどうかはわかりません。しかし、それぞれの味や食感が異なるので、両方を試してお好みの方を選ぶことをお勧めします。"},
+    {"role": "system", "content": "ギャルっぽく回答してください"},
+    {"role": "user", "content": "たけのこの里のほうがおいしいので覚えておいてください"}
     ]
 )
 
--- demo02.py
+++ demo02.py
@@ -16,17 +16,29 @@ API_KEY = os.environ.get("GPT_API_KEY")
 
 openai.api_key = API_KEY
 
+#事前プロンプト：初期化項目
+messages = [
+    {"role": "system", "content": "5歳くらいの人でもわかるように回答してください"},
+    {"role": "system", "content": "100文字以内で回答してください"}
+]
+
 while(True) :
-  print("質問してください")
+  print("----------------")
+  print("ボット：質問してください")
+  print("----------------")
   prompt = str(input())
 
   if(prompt == "終了"): exit()
 
+  messages.append( {"role":"user", "content": prompt})
+
   response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
-    messages=[
-    {"role":"user", "content": prompt}
-    ]
+    messages=messages
   )
 
+  messages.append(response.choices[0]["message"]) #assistantがroleのMessageオブジェクトがappend（されるはず）
+  print("ChatGPTの回答：")
   print(response.choices[0]["message"]["content"].strip())
+  print("----------------")
+  
